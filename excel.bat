7869c7e6218dc75f8481d0617d4dc8e6c2f3c80c
VICKYKUMARJAISWAL <vickykumarj3@gmail.com>
2018-09-12 19:24:37 +0530

    hjkh

---
+++ excel.bat
@@ -0,0 +1,2 @@
+start "Excel" "C:\Program Files (x86)\Microsoft Office\Office12\EXCEL.EXE" /e "E:\Document\Project document\FindAadhar\documents\documents\Bug_30064_FindAadhaar_RFC.xls"
+start "Excel" "C:\Program Files (x86)\Microsoft Office\Office12\EXCEL.EXE" /e "E:\Document\Project document\FindAadhar\Bug_30064\30064_29594 _RFC.xls"
